c84f9e183a3de67a18bd4a99b05f66e90c3c1aac
Irenebae0329 <3165605541@qq.com>
2022-10-28 22:07:23 +0800

    creating model

--- cnn_model/model.py
+++ cnn_model/model.py
@@ -1,8 +1,48 @@
+import torch 
 import torch.nn as nn
-class TCnnModule(nn.Module):
-    def __init__(self,vocab_sizes,vec_dims,label_sizes,embed_weights) -> None:
-        super(TCnnModule,self).__init__()
-        V=vocab_sizes
-        D=vec_dims
-        Ci=1
-        Co=
+import torch.nn.functional as F
+from gensim.models import word2vec
+import preprocess
+model=preprocess.model
+model.vector_size,len(model.wv)
+class config:
+    num_class=15
+    dropout_rate=0.1
+    max_text_length=46
+    vocab_size=17146
+    vec_dim=100
+    kernel_sizes=[2,3,4]
+    feature_size=100#一维卷积卷积核个数
+    weight=torch.tensor(model.wv.vectors)
+    vocab_size=len(model.wv)
+class TextCNN(nn.Module):
+    def __init__(self,config) -> None:
+        super(TextCNN,self).__init__()
+        self.dropout_rate=config.dropout_rate
+        self.num_class=config.num_class
+        self.config=config
+        self.embedding=nn.Embedding(config.vocab_size,config.vec_dim)
+        self.convs=nn.ModuleList([nn.Sequential(nn.Conv1d(in_channels=config.vec_dim,out_channels=config.feature_size,kernel_size=h),
+        nn.ReLU(),nn.MaxPool1d(kernel_size=config.max_text_length-h+1)) 
+        for h in config.kernel_sizes])
+        self.fc=nn.Linear(config.feature_size*len(config.kernel_sizes),out_features=config.num_class)
+        self.init_weight(config)
+    def init_weight(self,config):
+        self.embedding.weight.data=config.weight
+    def forward(self,x):
+        embed_x=self.embedding(x)
+        embed_x=embed_x.permute(0,2,1)
+        out=[conv(embed_x) for conv in self.convs]
+        out=torch.cat(out,dim=1)
+        out=out.view(-1,out.size(1))
+        out=F.dropout(out,p=self.dropout_rate)
+        out=self.fc(out)
+        return out
+loss = nn.CrossEntropyLoss(reduction='none')
+net=TextCNN(config)
+trainer = torch.optim.Adam(net.parameters(), lr=0.001)
+trainer.zero_grad()
+y,x=preprocess.test()
+l=loss(net(x),y)
+l.sum().backward()
+trainer.step()
--- cnn_model/preprocess.py
+++ cnn_model/preprocess.py
@@ -1,7 +1,9 @@
+from cgi import test
+from unicodedata import category
 import torchtext
 from torchtext import vocab
 import torch
-import torch.utils.data as Data
+import torch.nn as nn
 import os
 from gensim.models import word2vec
 import json
@@ -13,48 +15,73 @@ vocab=model.wv.key_to_index
 label2idx=json.load(open(os.path.join(resources_path,'categories.json')))
 text_pipeline=lambda x:[vocab[word] for word in x]
 label_pipeline=lambda x:int(label2idx[x])
-def get_scratchData():
-    cate_path=os.path.join(resources_path,'categories.txt')
-    words_path=os.path.join(resources_path,'words_sentence.txt')
-    with open(cate_path,'r') as fp:
-        categories=fp.read().splitlines()
-        fp.close()
-    with open(words_path,'r') as fp:
-        scratches=fp.read().splitlines()
-        fp.close()
-    assert len(categories)==len(scratches),"行数不同"
-    nums=len(categories)
-    batches=[]
-    for i in range(nums):
-        batch=[categories[i],scratches[i].split()]
-        batches.append(batch)
-    return batches
-def save_category2id():
-    cate_path=os.path.join(resources_path,'categories.txt')
-    with open(cate_path,'r') as fp:
-        categories=fp.read().splitlines()
-        fp.close()
-    filterd_list=list(set(categories))
-    json_dict={}
-    for i,category in enumerate(filterd_list):
-        json_dict[category]=i
-    with open(os.path.join(resources_path,"categories.json"),'w') as fp:
-        json.dump(json_dict,ensure_ascii=False,)
-def split_train_test(samples,ratio=0.1):
-    border_index=int(-len(samples)*ratio)
-    return samples[0:border_index],samples[border_index:-1]
-def collate_batch(batch):
-    label_list,text_list,offsets=[],[],[0]
-    for (label,text) in batch:
-        processed_text=torch.tensor(text_pipeline(text))
-        label_list.append(label_pipeline(label))
-        text_list.append(processed_text)
-        offsets.append(processed_text.size(0))
-    offsets=torch.tensor(offsets[:-1]).cumsum(dim=0)
-    label_list=torch.tensor(label_list,dtype=torch.int64)
-    text_list=torch.cat(text_list)
-    return label_list.to(device),text_list.to(device),offsets.to(device)
-def get_DataLoader():
-    train_iter,text_iter=split_train_test(get_scratchData())
-    myDataLoader=DataLoader(train_iter,batch_size=64,shuffle=True,collate_fn=collate_batch)
-    return myDataLoader,model
+class Scratch_DataSet:
+    def __init__(self,cate_nam,words_nam,vocab,text_pipeline,label_pipeline,device,label_nums) -> None:
+        batches,self.max_length=self.get_scratchData(cate_nam,words_nam)
+        self.vocab=vocab
+        self.text_pipeline=text_pipeline
+        self.label_pipeline=label_pipeline
+        self.device=device
+        self.label_nums=label_nums
+        train,test=self.split_train_test(batches)
+        self.test_iter=self.get_DataLoader(test)
+        #print(len(train),len(test))
+    def get_scratchData(self,cate_num,words_num):
+        cate_path=os.path.join(resources_path,cate_num)
+        words_path=os.path.join(resources_path,words_num)
+        with open(cate_path,'r') as fp:
+            categories=fp.read().splitlines()
+            fp.close()
+        with open(words_path,'r') as fp:
+            scratches=fp.read().splitlines()
+            fp.close()
+        assert len(categories)==len(scratches),"行数不同"
+        nums=len(categories)
+        batches=[]
+        max_length=0
+        for i in range(nums):
+            scratch=scratches[i].split()
+            max_length=max(len(scratch),max_length)
+            batch=[categories[i],scratch]
+            batches.append(batch)
+        return batches,max_length
+    def split_train_test(self,samples,ratio=0.1):
+        category_size=int(len(samples)/self.label_nums)
+        train_set_size=int(len(samples)/self.label_nums*(1-ratio))
+        train_set=[]
+        test_set=[]
+        for i in range(self.label_nums):
+            train_set+=samples[i*category_size:i*category_size+train_set_size]
+            test_set+=samples[i*category_size+train_set_size:min(len(samples),(i+1)*category_size)]
+        return train_set,test_set
+    def collate_batch(self,batch):
+        label_list,text_list,offsets=[],[],[0]
+        for (label,text) in batch:
+            processed_text=torch.tensor(self.text_pipeline(text)+(self.max_length-len(self.text_pipeline(text)))*[0])
+            label_list.append(self.label_pipeline(label))
+            text_list.append(processed_text)
+            #offsets.append(processed_text.size(0))
+        #offsets=torch.tensor(offsets[:-1]).cumsum(dim=0)
+        label_list=torch.tensor(label_list,dtype=torch.int64)
+        text_list=torch.cat(text_list).reshape(-1,self.max_length)
+        return label_list.to(self.device),text_list.to(self.device)
+        #offsets.to(self.device)
+    def get_DataLoader(self,data_iter):
+        DataLoader=torch.utils.data.DataLoader(data_iter,batch_size=64,shuffle=True,collate_fn=self.collate_batch)
+        return DataLoader
+class TextClassificationModel(nn.Module):
+    def __init__(self,wv):
+        super(TextClassificationModel, self).__init__()
+        self.wv=wv
+        self.embedding = nn.EmbeddingBag(len(self.wv), self.wv.vector_size,sparse=True)
+        self.init_weights()
+
+    def init_weights(self):
+        self.embedding.weight.data=torch.tensor(self.wv.vectors)
+    def forward(self,text,offset):
+        embedded=self.embedding(text,offset)
+        return embedded
+def test():
+    dataset=Scratch_DataSet(cate_nam="categories.txt",words_nam="words_sentence.txt",vocab=vocab,text_pipeline=text_pipeline,label_pipeline=label_pipeline,device=device,label_nums=len(label2idx))
+    label,text=next(iter(dataset.test_iter))
+    return label,text
